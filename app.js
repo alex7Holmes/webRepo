48064f8638f536c8c62932618506e41031aa8553
Alex Holmes <holmes5788@gmail.com>
2021-03-24 19:20:11 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
+const app = express();
+
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart() {
+    console.log("Express server listening on: " + HTTP_PORT);
+}
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"))
+})
+
+app.use(function(req, res, next) {
+    res.status(404).send("404: Page Not Found");
+});
+
+app.listen(HTTP_PORT, onHttpStart);
---
+++ home.js
@@ -0,0 +1,35 @@
+// "month": "3",
+// "num": 2440,
+// "link": "",
+// "year": "2021",
+// "news": "",
+// "safe_title": "Epistemic Uncertainty",
+// "transcript": "",
+// "alt": "Luckily, unlike in our previous study, we have no reason to believe Evangeline the Adulterator gained access to our stored doses.",
+// "img": "https://imgs.xkcd.com/comics/epistemic_uncertainty.png",
+// "title": "Epistemic Uncertainty",
+// "day": "22"
+
+import axios from 'axios';
+
+
+var baseURL = "http://xkcd.com/info.0.json";
+var baseImageUrl = null;
+var configData = null;
+
+//function to get data from API
+const getComics = () => {
+    axios.get(baseURL)
+    .then(response => {
+        configData = response.data;
+        baseImageUrl = response.data.img;
+        const comicTitle = response.data.title;
+        console.log(allData);
+        console.log('COMICS', comicImage);
+        document.querySelector("#comics").innerHTML = comicImage;
+    })
+    .catch(err => console.log(err));
+}
+
+getComics();
+
