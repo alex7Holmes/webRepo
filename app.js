831612e7ce9f2f6cf202f5a96f9142d92352980f
Alex Holmes <holmes5788@gmail.com>
2021-03-26 18:26:00 -0400

    commit

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, "/views/about.html"))
-})
+});
 
 app.use((req, res, next) => {
     res.status(404).send("404: Page Not Found");
